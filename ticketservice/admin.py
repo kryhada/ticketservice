be2e83202c13717b89b524a2ad46313d65e7dfaf
root <root@kryhada.nazwa.pl>
2018-04-26 22:11:42 +0200

    update models

--- ticketservice/admin.py
+++ ticketservice/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Address, Area, Discount, Event, Event_type, Role, Sales, Ticket, Ticket_type, User, User_details
 
-# Register your models here.
+admin.site.register(Address)
+admin.site.register(Area)
+admin.site.register(Discount)
+admin.site.register(Event)
+admin.site.register(Event_type)
+admin.site.register(Role)
+admin.site.register(Sales)
+admin.site.register(Ticket)
+admin.site.register(Ticket_type)
+admin.site.register(User)
+admin.site.register(User_details)
--- ticketservice/models.py
+++ ticketservice/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+class Address(models.Model):
+    postal_code = models.CharField(max_length=30)
+    street = models.CharField(max_length=30)
+    locality = models.CharField(max_length=30)
+    number = models.CharField(max_length=4)
+
+class User_details(models.Model):
+    id_address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    phone = models.CharField(max_length=9)
+
+class Role(models.Model):
+    role = models.CharField(max_length=30)
+
 class User(models.Model):
     id_user_details = models.ForeignKey(User_details, on_delete=models.CASCADE)
     id_role = models.ForeignKey(Role, on_delete=models.CASCADE)    
@@ -10,18 +25,33 @@ class User(models.Model):
     salt = models.IntegerField()
     created_at = models.DateField()
 
-class User_details(models.Model):
-    id_address = models.ForeignKey(Address, on on_delete=models.CASCADE)
+class Event_type(models.Model):
     name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
-    phone = models.CharField(max_length=9)
 
-class Role(models.Model):
-    role = models.CharField(max_length=30)
+class Area(models.Model):
+    name = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    capacity = models.IntegerField()
 
-class Address(models.Model):
-    postal_code = models.CharField(max_length=30)
-    street = models.CharField(max_length=30)
-    locality = models.CharField(max_length=30)
-    number = models.CharField(max_length=4)
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    event_type = models.ForeignKey(Event_type, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
 
+class Discount(models.Model):
+    discount = models.FloatField()
+
+class Ticket_type(models.Model):
+    name = models.CharField(max_length=30)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+
+class Ticket(models.Model):
+    number = models.CharField(max_length=30)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    price = models.FloatField()
+    ticket_type = models.ForeignKey(Ticket_type, on_delete=models.CASCADE)
+    
+class Sales(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    
