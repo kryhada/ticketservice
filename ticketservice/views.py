0ec6ff5268260e4eb9584bea639f3bc38fc8174d
root <root@kryhada.nazwa.pl>
2018-05-11 20:23:48 +0200

    testing index page

--- ticketservice/views.py
+++ ticketservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ticketservice.models import User 
 
 def index(request):
-	return render(request, 'index.html') 
+	return render(request, 'index.html', {'users' : User.objects.all()}) 
